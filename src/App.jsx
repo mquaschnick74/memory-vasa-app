628253923a6655a28644fca93b9e3b5d2d52547d
mquaschnick74 <131696297+mquaschnick74@users.noreply.github.com>
2025-06-09 14:58:55 -0500

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import VASAInterface from './VoiceAgent';
-
-function App() {
-  return <VASAInterface />;
-}
-
-export default App;
--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import VASAInterface from './VoiceAgent.jsx'
-import './App.css'
-
-createRoot(document.getElementById('root')).render(<VASAInterface />)
