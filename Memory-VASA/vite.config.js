e28612b045ff6670b01fd17126dcd90d6ed0ed55
mquaschnick74 <131696297+mquaschnick74@users.noreply.github.com>
2025-06-14 09:07:03 -0500

    Update vite.config.js

--- Memory-VASA/vite.config.js
+++ Memory-VASA/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ['VITE_'],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
